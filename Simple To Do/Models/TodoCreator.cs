c929b596a81ef38b14ccb6a755474ba3f9f9911b
golfingtrex <77211832+golfingtrex@users.noreply.github.com>
2023-01-03 20:52:21 -0500

    Add project files.

---
+++ Simple To Do/Models/TodoCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Simple_To_Do.Models
+{
+    internal class TodoCreator
+    {
+        private Button submitTodoButton = new();
+        public Button SubmitTodoButton => submitTodoButton;
+
+        private TextBox todoTextBox = new();
+        public TextBox TodoTextBox => todoTextBox;
+    }
+}
