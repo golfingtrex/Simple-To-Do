135b866de2d542544be8642848186495bc370f31
golfingtrex <77211832+golfingtrex@users.noreply.github.com>
2023-01-07 18:34:10 -0500

    Improved code styling by adding StaticResources to Application.Resources. Everything is basically fine except the TodoListBoxItem template.

---
+++ Simple To Do/ViewModels/TodoEntryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace Simple_To_Do.ViewModels
+{
+    internal class TodoEntryViewModel : ViewModel
+    {
+        private string todoName;
+        public string TodoName
+        {
+            get
+            {
+
+                return todoName;
+            }
+            set
+            {
+                todoName = value;
+                OnPropertyChanged(nameof(TodoName));
+            }
+        }
+
+        public ICommand TodoEntryCommand { get; }
+
+        public TodoEntryViewModel()
+        {
+
+        }
+    }
+}
---
+++ Simple To Do/ViewModels/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Simple_To_Do.ViewModels
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
