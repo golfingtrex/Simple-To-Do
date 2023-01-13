e4e970ac388a27b10c43dca751e21dad8ca18fed
golfingtrex <77211832+golfingtrex@users.noreply.github.com>
2023-01-12 19:05:30 -0500

    I was attempting to do some sleek MVVM here, but it's a bit over my head right now. I'm going to commit these changes (messy) as they are, and make a new branch where I do not attempt any MVVM and just work out of the MainWindow.

--- Simple To Do/Views/TodoEntryView.xaml.cs
+++ Simple To Do/Views/TodoEntryView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Simple_To_Do.Views
 {
@@ -23,6 +11,39 @@ namespace Simple_To_Do.Views
         public TodoEntryView()
         {
             InitializeComponent();
+        }
+
+        private void TodoEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            //// Get the parent window of the current view
+            //var parentWindow = Window.GetWindow(this);
+
+            //// Find the ListBox in the other view by its name
+            //var otherViewListBox = FindName("TodoList") as ListBox;
+            //if (otherViewListBox != null)
+            //{
+            //    // Get the string from the TextBox
+            //    var text = abc.Text;
+            //    // Add the string to the ListBox
+            //    otherViewListBox.Items.Add(text);
+            //}
+
+            //LB.Items.Add(mainStack.FindName("abc").ToString());
+
+            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+            var control = mainWindow.TryFindResource("TodoListView");
+
+
+            //Views.TodoListView todoListView = (TodoListView)this.FindName("TodoListView");
+            //var control = todoListView.FindName("TodoList") as ListBox;
+
+            if (true)
+            {
+                int x = 5;
+                x++;
+            }
+
+            //control?.Items.Add("hi");
         }
     }
 }
