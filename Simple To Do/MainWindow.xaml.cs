3fc7112407563297a2a98bda9f3885bfaf08bf20
golfingtrex <77211832+golfingtrex@users.noreply.github.com>
2023-01-12 21:25:19 -0500

    Did everything from MainWindow instead of using MVVM and App.xaml to store resources. I was able to get the layout I desire, but I still need to add functionality to the CheckBox and Button for the ListBox items.

--- Simple To Do/MainWindow.xaml.cs
+++ Simple To Do/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Simple_To_Do
 {
@@ -23,6 +13,78 @@ namespace Simple_To_Do
         public MainWindow()
         {
             InitializeComponent();
+            TodoEntryTextBox.Focus();
+        }
+
+        //TODO Find out more about RoutedEventArgs
+        /// <summary>
+        /// Checks if the TodoEntryTextBox has text. If it does, create a CheckBox, TextBlock and Button with desired values.
+        /// Then add each of those UIElements to a Horizontal Stack Panel. Then create a Grid with 3 columns such that any
+        /// UIElement placed in the center column will be centered in the window. Then add that Grid to the TodoListBox.
+        /// </summary>
+        /// <param name="sender">The Add Todo button on the Main Window.</param>
+        /// <param name="e"> //TODO I'm not sure what this is honestly...</param>
+        private void AddTodoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TodoEntryTextBox.Text))
+            {
+                CheckBox checkBox = new()
+                {
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                TextBlock textBlock = new()
+                {
+                    Text = TodoEntryTextBox.Text,
+                    TextWrapping = TextWrapping.Wrap,
+                    Width = 500,
+                    //Height = 30, // disable for TextWrapping
+                    FontSize = 18,
+                    Padding = new Thickness(2),
+                    Margin = new Thickness(10, 0, 0, 0),
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                Button button = new()
+                {
+                    Content = "X",
+                    Width = 30,
+                    Height = 30,
+                    Background = new SolidColorBrush(Colors.Red)
+                };
+
+                StackPanel stackPanel = new()
+                {
+                    Orientation = Orientation.Horizontal
+                };
+                stackPanel.Children.Add(checkBox);
+                stackPanel.Children.Add(textBlock);
+                stackPanel.Children.Add(button);
+
+                Grid grid = new();
+                ColumnDefinition col0 = new()
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                };
+                ColumnDefinition col1 = new()
+                {
+                    Width = new GridLength(0, GridUnitType.Auto)
+                };
+                ColumnDefinition col2 = new()
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                };
+                grid.ColumnDefinitions.Add(col0);
+                grid.ColumnDefinitions.Add(col1);
+                grid.ColumnDefinitions.Add(col2);
+                grid.Children.Add(stackPanel);
+                Grid.SetColumn(stackPanel, 1);
+
+                TodoListBox.Items.Add(grid);
+            }
+
+            TodoEntryTextBox.Clear();
         }
     }
 }
